8b921125e0bba31cfb9e78a5b5c8e187b044920d
Alexis <alexiskokamura@gmail.com>
2018-04-19 17:29:29 -1000

    styled first page

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,74 +1,66 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, Dimensions, TouchableOpacity } from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
+import { StyleSheet, Image, View } from 'react-native';
+import { Container, Button, Text } from 'native-base';
+import { LinearGradient } from 'expo';
 
 class HomePage extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      text: ``
-    }
+      text: ''
+    };
   }
-  
+
   render() {
     return (
-      // <Container navigation={this.props.navigation}>
-        <View style={styles.container}>
-          <Image source={require('../../../parklinq.png')}
-            style={{ alignSelf: 'center',
-            height: 350,
-            width: 350,
-            bottom: 220 }} />
-          <TouchableOpacity
-            style={styles.button1}
-            onPress={() => this.props.navigation.navigate(`Login`)}>
-            {/* <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.linearGradient}> */}
-            <Text style={{ color: `lightgrey` }}>Login</Text>
-            {/* </LinearGradient> */}
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button2}
-            onPress={() => this.props.navigation.navigate(`Register`)}>
-            <Text style={{ color: `white` }}>Sign Up</Text>
-          </TouchableOpacity>
-        </View>
-       
+      <LinearGradient
+        colors={['#39c1ef', '#1b8ebe']}
+        style={[styles.flex, styles.background]}
+        start={[0,0]}
+        end={[1,0]}
+      >
+        <Container style={[styles.flex]}>
+          <Image
+            source={require('../../assets/logo_white.png')}
+            style={{
+              height: 100,
+              width: 300,
+              marginBottom: 75
+            }}
+            resizeMode="contain"
+          />
+          <Button style={[styles.button]} block light>
+            <Text style={[styles.buttonText]}>Create an Account</Text>
+          </Button>
+          <Button style={[styles.button]} block light>
+            <Text style={[styles.buttonText]}>Log In</Text>
+          </Button>
+        </Container>
+      </LinearGradient>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-  flex: 1,
-  backgroundColor: 'black',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
-  paddingBottom: 50
+  flex: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    alignContent: 'center',
+    justifyContent: 'center'
   },
-  button1: {
-  justifyContent: `center`,
-  alignItems: `center`,
-  height: 40,
-  margin: 10,
-  width: 250,
-  borderColor: `lightgrey`,
-  borderWidth: 2,
-  borderStyle: `solid`,
-  borderRadius: 5
+  background: {
+    padding: 30
   },
-  button2: {
-  justifyContent: `center`,
-  alignItems: `center`,
-  height: 40,
-  margin: 10,
-  width: 250,
-  backgroundColor: `powderblue`,
-  borderWidth: 1,
-  borderStyle: `solid`,
-  borderRadius: 5
+  button: {
+    marginBottom: 15
+  },
+  buttonText: {
+    color: '#39c1ef',
+    fontWeight: 'bold'
   }
-  });
-  
+});
+
 
 export default HomePage;
